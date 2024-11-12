55ccf9dbece3272baa73e63ebaa62bfb94b82b59
Kazuya Takei <myself@attakei.net>
2024-11-12 16:21:51 +0900

    feat: Render as html from markdown (Zenn style)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Buffer } from "node:buffer";
 import { Hono } from "hono";
 import { Octokit } from "@octokit/rest";
+import markdownToHtml from "zenn-markdown-html";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
@@ -14,7 +15,22 @@ app.get("/:slug", async (c) => {
     path: `articles/${c.req.param("slug")}.md`,
   });
   const md = Buffer.from(resp.data.content, "base64").toString();
-  return c.text(md);
+  const html = `
+  <!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Zenn article</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/zenn-content-css@0.1.158/lib/index.min.css">
+</head>
+<body>
+  <div class="znc">
+    ${markdownToHtml(md)}
+  </div>
+</body>
+</html>
+  `;
+  return c.html(html);
 });
 
 export default app;

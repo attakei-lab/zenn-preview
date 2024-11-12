faef93ddef2b690d7d44debc6f7670acd796b2c6
Kazuya Takei <myself@attakei.net>
2024-11-12 16:12:30 +0900

    feat: Fetch markdown content for html source

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { Hono } from 'hono'
+import { Buffer } from "node:buffer";
+import { Hono } from "hono";
+import { Octokit } from "@octokit/rest";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>()
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/:slug", async (c) => {
+  const octokit = new Octokit({
+    auth: c.env.REPO_PAT,
+  });
+  const resp = await octokit.rest.repos.getContent({
+    owner: c.env.REPO_OWNER,
+    repo: c.env.REPO_NAME,
+    path: `articles/${c.req.param("slug")}.md`,
+  });
+  const md = Buffer.from(resp.data.content, "base64").toString();
+  return c.text(md);
+});
 
-export default app
+export default app;

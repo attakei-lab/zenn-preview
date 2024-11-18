9e3425646ae067ef6ba38c09f759e96b80643995
Kazuya Takei <myself@attakei.net>
2024-11-18 11:13:09 +0900

    feat: Remove front-matter items from generated html

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 import { Buffer } from 'node:buffer';
 import { Context, Hono } from 'hono';
+import { parse, HTMLElement } from 'node-html-parser';
+import frontMatter from 'front-matter';
 import { sentry } from '@hono/sentry';
 import { Octokit } from '@octokit/rest';
 import markdownToHtml from 'zenn-markdown-html';
 
 type Props = {
   body?: string;
+};
+
+type FrontMatter = {
+  title?: string;
+  emoji?: string;
+  topics?: string[];
+  type?: string;
+  published?: boolean;
+  published_at?: string;
+};
+
+const parseFrontMatter = (
+  elm: HTMLElement,
+  pre?: (e: HTMLElement) => HTMLElement,
+): FrontMatter => {
+  const raw = (pre ? pre(elm) : elm).innerHTML;
+  const source = `---\n${raw}\n---\n`;
+  const fm = frontMatter(source);
+  return fm.attributes as FrontMatter;
 };
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
@@ -31,7 +52,20 @@ app.get('/:slug', async (c) => {
   try {
     const resp = await octokit.rest.repos.getContent(params);
     const md = Buffer.from(resp.data.content, 'base64').toString();
-    props.body = markdownToHtml(md);
+    const dom = parse(markdownToHtml(md));
+    dom.querySelector('hr')?.remove();
+    const metadata = dom.querySelector('h2');
+    metadata?.parentNode?.removeChild(metadata);
+    const frontMatter = parseFrontMatter(metadata, (elm) => {
+      elm.querySelector('a').remove();
+      for (const br of elm.querySelectorAll('br')) {
+        br.parentNode.removeChild(br);
+      }
+      return elm;
+    });
+
+    console.log(frontMatter);
+    props.body = dom.toString();
   } catch (error) {
     if (error.status) {
       console.error(error);

3823061b36ca2cc8d0af9b9c55034e3a25b3ceef
Kazuya Takei <myself@attakei.net>
2024-11-22 01:09:22 +0900

    feat: First definition to create slug of url

---
+++ src/models.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+/**
+ * Content information for reproduce.
+ */
+export const ContentAddress = z.object({
+  owner: z.string(),
+  repo: z.string(),
+  path: z.string().endsWith('.md'),
+  ref: z.optional(z.string()),
+});
+
+export type ContentAddress = z.infer<typeof ContentAddress>;
+
+/**
+ * Generate slug text for URL of contents.
+ */
+export const makeSlug = (addr: ContentAddress): string => {
+  const json = JSON.stringify(addr);
+  return btoa(encodeURIComponent(json));
+};
+
+/**
+ * Reverse address object from slug text.
+ */
+export const parseSlug = (slug: string): ContentAddress => {
+  const json = decodeURIComponent(atob(slug));
+  return ContentAddress.parse(JSON.parse(json));
+};

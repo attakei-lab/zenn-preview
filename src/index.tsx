43ddb10bcea7ab600d9c60bb1c6670c39021dd6f
Kazuya Takei <myself@attakei.net>
2024-11-24 18:43:03 +0900

    refactor: Remove unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
 import type { Context } from 'hono';
 import { Hono } from 'hono';
-import { html, raw } from 'hono/html';
 import { sentry } from '@hono/sentry';
-import { Octokit } from '@octokit/rest';
 import api from './api';
 import htmlRouter from './html';
-import { fetchContent } from './client';
-import { parseContentMarkdown } from './parser';
-import { parseSlug } from './models';
-import type { ZennContent } from './models';
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 

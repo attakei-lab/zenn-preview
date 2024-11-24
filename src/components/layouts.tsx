8325c332991651a1426889bee7ae209fb86428b6
Kazuya Takei <myself@attakei.net>
2024-11-24 20:06:38 +0900

    refactor: Wrap layout components

---
+++ src/components/layouts.tsx
@@ -0,0 +1,44 @@
+import type { FC, PropsWithChildren } from 'hono/jsx';
+
+type LayoutProps = {
+  title: string;
+  cssUrls?: string[];
+  jsUrls?: string[];
+};
+
+export const Layout: FC<PropsWithChildren<LayoutProps>> = (props) => {
+  return (
+    <html lang="ja">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{props.title}</title>
+        {props.cssUrls?.map((css) => (
+          <link rel="stylesheet" href={css} key={css} />
+        ))}
+      </head>
+      <body>{props.children}</body>
+    </html>
+  );
+};
+
+export const AppLayout: FC<
+  PropsWithChildren<{
+    title: string;
+  }>
+> = (props) => {
+  const cssFiles = [
+    'https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css',
+  ];
+  const jsFiles = [
+    'https://unpkg.com/htmx.org@2.0.3',
+    'https://unpkg.com/htmx-ext-json-enc@2.0.1/json-enc.js',
+    'https://unpkg.com/htmx-ext-client-side-templates@2.0.0/client-side-templates.js',
+    'https://unpkg.com/mustache@latest',
+  ];
+  return (
+    <Layout title={props.title} cssUrls={cssFiles} jsUrls={jsFiles}>
+      {props.children}
+    </Layout>
+  );
+};
--- src/routes/html.tsx
+++ src/routes/html.tsx
@@ -7,8 +7,8 @@ import { Hono } from 'hono';
 import { Octokit } from '@octokit/rest';
 import { fetchContent } from '../client';
 import { Input } from '../components';
+import { AppLayout, Layout } from '../components/layouts';
 import { parseSlug } from '../models';
-import type { ZennContent } from '../models';
 import { parseContentMarkdown } from '../parser';
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
@@ -24,107 +24,87 @@ app.use(async (c, next) => {
  */
 app.get('/', (c) => {
   return c.render(
-    <html lang="ja">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Zenn Private Previwer</title>
-        <script src="https://unpkg.com/htmx.org@2.0.3" />
-        <script src="https://unpkg.com/htmx-ext-json-enc@2.0.1/json-enc.js" />
-        <script src="https://unpkg.com/htmx-ext-client-side-templates@2.0.0/client-side-templates.js" />
-        <script src="https://unpkg.com/mustache@latest" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
-        />
-      </head>
-      <body>
-        <section class="hero">
-          <div class="hero-body">
-            <h1 class="title">Zenn Private Previwer</h1>
-          </div>
-        </section>
-        <section class="section">
-          <form
-            hx-post="/api/content-url"
-            hx-ext="json-enc, client-side-templates"
-            hx-target="#result"
-            hx-swap="innerHTML"
-            mustache-template="tmpl-result"
-          >
-            <Input name="owner" label="ユーザー/Org" />
-            <Input name="repo" label="リポジトリ" />
-            <Input name="path" label="ファイルパス" />
-            <Input name="ref" label="ブランチ" />
-            <div class="field is-horizontal">
-              <div class="field-label" />
-              <div class="field-body">
-                <div class="field">
-                  <p class="control">
-                    <button class="button is-success" type="submit">
-                      URL生成
-                    </button>
-                  </p>
-                </div>
+    <AppLayout title="Zenn Private Previwer">
+      <section class="hero">
+        <div class="hero-body">
+          <h1 class="title">Zenn Private Previwer</h1>
+        </div>
+      </section>
+      <section class="section">
+        <form
+          hx-post="/api/content-url"
+          hx-ext="json-enc, client-side-templates"
+          hx-target="#result"
+          hx-swap="innerHTML"
+          mustache-template="tmpl-result"
+        >
+          <Input name="owner" label="ユーザー/Org" />
+          <Input name="repo" label="リポジトリ" />
+          <Input name="path" label="ファイルパス" />
+          <Input name="ref" label="ブランチ" />
+          <div class="field is-horizontal">
+            <div class="field-label" />
+            <div class="field-body">
+              <div class="field">
+                <p class="control">
+                  <button class="button is-success" type="submit">
+                    URL生成
+                  </button>
+                </p>
               </div>
             </div>
-          </form>
-        </section>
-        <section class="section" id="result" />
-        <template id="tmpl-result">
-          <article class="message is-link">
-            <div class="message-header">
-              <p>OK!</p>
-            </div>
-            <div class="message-body">
-              <p>Content URL is created!</p>
-              <p>
-                Link is{' '}
-                <a href="/{{slug}}" target="_blank" rel="noreferrer">
-                  here
-                </a>
-                .
-              </p>
-            </div>
-          </article>
-        </template>
-      </body>
-    </html>,
+          </div>
+        </form>
+      </section>
+      <section class="section" id="result" />
+      <template id="tmpl-result">
+        <article class="message is-link">
+          <div class="message-header">
+            <p>OK!</p>
+          </div>
+          <div class="message-body">
+            <p>Content URL is created!</p>
+            <p>
+              Link is{' '}
+              <a href="/{{slug}}" target="_blank" rel="noreferrer">
+                here
+              </a>
+              .
+            </p>
+          </div>
+        </article>
+      </template>
+    </AppLayout>,
   );
 });
 
 app.get('/:slug', async (c) => {
-  let props: ZennContent;
   const octokit = new Octokit({
     auth: c.env.REPO_PAT,
   });
   const addr = parseSlug(c.req.param('slug'));
   try {
     const md = await fetchContent(octokit, addr);
-    props = parseContentMarkdown(md);
+    const content = parseContentMarkdown(md);
+    const cssUrls = [
+      'https://cdn.jsdelivr.net/npm/zenn-content-css@0.1.158/lib/index.min.css',
+    ];
     return c.render(
-      <html lang="ja">
-        <head>
-          <meta charset="UTF-8" />
-          <title>Zenn article</title>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/zenn-content-css@0.1.158/lib/index.min.css"
-          />
-        </head>
-        <body>
-          <div>
-            <h1>
-              {props.frontMatter?.title}
-              <br />
-              {props.frontMatter?.emoji}
-            </h1>
-          </div>
-          <hr />
-          <hr />
-          <div class="znc" dangerouslySetInnerHTML={{ __html: props.body }} />
-        </body>
-      </html>,
+      <Layout
+        title={`[PREVIRE]: ${content.frontMatter?.title}`}
+        cssUrls={cssUrls}
+      >
+        <div>
+          <h1>
+            {content.frontMatter?.title}
+            <br />
+            {content.frontMatter?.emoji}
+          </h1>
+        </div>
+        <hr />
+        <hr />
+        <div class="znc" dangerouslySetInnerHTML={{ __html: content.body }} />
+      </Layout>,
     );
   } catch (error) {
     console.error(error);
